bf06488f1915156c9c073e45ffc9471e819e9f83
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 16:03:00 -0500

    Create file_operation.cpp

---
+++ src/file_operation.cpp
@@ -0,0 +1,24 @@
+#include "file_io.h"
+
+
+static void create_new_file(const char *path, mode_t mode) {
+    /* 
+    TODO
+    Check if file already exist
+    */
+    int file_to_create = creat(path,mode);
+    if (file_to_create != -1) {
+        std::cout<<"["<<path<<"] created successfully with: "<<mode<<" Descriptor"<<std::endl;
+    } else {
+        std::cout<<"Error creating filename: "<<path<<std::endl;
+    }
+}
+
+static int close_opened_file(int fd) {
+    int closed_fd = close(fd);
+    if(closed_fd == 0) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
