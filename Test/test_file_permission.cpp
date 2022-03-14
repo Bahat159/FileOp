fe6e3e27aed0e24be7c0358e49fe687a85008c70
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 16:05:27 -0500

    Create test_file_permission.cpp

---
+++ Test/test_file_permission.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "file_io.h"
+
+/*
+Author: Busari Habibullaah
+Year: March, 2022
+Copyright: SandCroft Software
+Project Type: Open-source 
+*/
+
+int main() {
+    std::cout<<"File I/O Operation Call" <<std::endl;
+  
+    check_file_permission_type(READ_ONLY);
+    check_file_descriptor(READ_ONLY);
+
+    return 0;
+}
