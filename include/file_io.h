df0e8f944b3f212fef09b39fece1192b4656ba53
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 15:58:34 -0500

    Update file_io.h

--- include/file_io.h
+++ include/file_io.h
@@ -36,18 +36,7 @@ Project Type: Open-source
 #endif
 
 
-static void check_file_permission_type(char file_perm) {
-    switch (file_perm)
-    {
-    case READ_ONLY:
-        std::cout <<"READ_ONLY"<<std::endl;
-        break;
-    
-    default:
-        std::cout <<"Unknown file permission type"<<std::endl;
-        break;
-    }
-}
+static void check_file_permission_type(char file_perm);
 
 static void check_file_descriptor(char file_perm) {
     if (file_perm == O_RDONLY){
