3760554e6c4d925c04b655db56b1802848a6dc05
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 18:17:28 -0500

    Update file_permission.cpp

--- src/file_permission.cpp
+++ src/file_permission.cpp
@@ -21,8 +21,23 @@ static void check_file_permission_type(char file_perm) {
     }
 }
 
-static void check_file_descriptor(char file_perm) {
-    if (file_perm == O_RDONLY){
-        std::cout<<"O_RDONLY File Descriptor: "<<READ_ONLY<<std::endl;
+static void check_file_descriptor(char fd) {
+    switch (fd)
+    {
+    case READ_ONLY:
+        std::cout<<"READ_ONLY File Descriptor: "<<READ_ONLY<<std::endl;
+        break;
+    case WRITE_ONLY:
+        std::cout<<"WRITE_ONLY File Descriptor: "<<WRITE_ONLY<<std::endl;
+        break;
+    case READ_WRITE:
+        std::cout<<"READ_WRITE File Descriptor: "<<READ_WRITE<<std::endl;
+        break;
+    case CREATE_ONLY:
+        std::cout<<"CREATE_ONLY File Descriptor: "<<CREATE_ONLY<<std::endl;
+        break;
+    default:
+        std::cout <<"Unknown file descriptor"<<std::endl;
+        break;
     }
 }
