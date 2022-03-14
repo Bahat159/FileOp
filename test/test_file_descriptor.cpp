1ef06b7f2a20d160062f27a63b7784295dc8c40a
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 18:18:33 -0500

    Create test_file_descriptor.cpp

---
+++ test/test_file_descriptor.cpp
@@ -0,0 +1,17 @@
+#include "../include/file_io.h"
+#include "../src/file_permission.cpp"
+
+/*
+Author: Busari Habibullaah
+Year: March, 2022
+Copyright: SandCroft Software
+Project Type: Open-source 
+*/
+
+int main() {
+    std::cout<<"File I/O Operation Call" <<std::endl;
+  
+    check_file_descriptor(CREATE_ONLY);
+
+    return 0;
+}
