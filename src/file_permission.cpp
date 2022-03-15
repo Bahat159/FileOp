39c1d9fc162bfa38eba882b62abbc152123aa201
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 21:54:35 -0500

    Update file_permission.cpp

--- src/file_permission.cpp
+++ src/file_permission.cpp
@@ -1,18 +1,18 @@
-#include "file_io.h"
+#include "../include/file_io.h"
 
 static void check_file_permission_type(char file_perm) {
     switch (file_perm)
     {
-    case READ_ONLY:
+    case FILE_IO_PERM_READ_ONLY:
         std::cout <<"READ_ONLY"<<std::endl;
         break;
-    case WRITE_ONLY:
+    case FILE_IO_PERM_WRITE_ONLY:
         std::cout <<"WRITE_ONLY"<<std::endl;
         break;
-    case READ_WRITE:
+    case FILE_IO_PERM_READ_WRITE:
         std::cout <<"READ_WRITE"<<std::endl;
         break;
-    case CREATE_ONLY:
+    case FILE_IO_PERM_CREATE_ONLY:
         std::cout <<"CREATE_ONLY"<<std::endl;
         break;
     default:
@@ -24,17 +24,17 @@ static void check_file_permission_type(char file_perm) {
 static void check_file_descriptor(char fd) {
     switch (fd)
     {
-    case READ_ONLY:
-        std::cout<<"READ_ONLY File Descriptor: "<<READ_ONLY<<std::endl;
+    case FILE_IO_PERM_READ_ONLY:
+        std::cout<<"READ_ONLY File Descriptor: "<<FILE_IO_PERM_READ_ONLY<<std::endl;
         break;
-    case WRITE_ONLY:
-        std::cout<<"WRITE_ONLY File Descriptor: "<<WRITE_ONLY<<std::endl;
+    case FILE_IO_PERM_WRITE_ONLY:
+        std::cout<<"WRITE_ONLY File Descriptor: "<<FILE_IO_PERM_WRITE_ONLY<<std::endl;
         break;
-    case READ_WRITE:
-        std::cout<<"READ_WRITE File Descriptor: "<<READ_WRITE<<std::endl;
+    case FILE_IO_PERM_READ_WRITE:
+        std::cout<<"READ_WRITE File Descriptor: "<<FILE_IO_PERM_READ_WRITE<<std::endl;
         break;
-    case CREATE_ONLY:
-        std::cout<<"CREATE_ONLY File Descriptor: "<<CREATE_ONLY<<std::endl;
+    case FILE_IO_PERM_CREATE_ONLY:
+        std::cout<<"CREATE_ONLY File Descriptor: "<<FILE_IO_PERM_CREATE_ONLY<<std::endl;
         break;
     default:
         std::cout <<"Unknown file descriptor"<<std::endl;
