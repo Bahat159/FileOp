04a267b6fa4af303512e553d60b0e5f0eacf56cf
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 20:41:47 -0500

    Update test_file_operation.cpp

--- test/test_file_operation.cpp
+++ test/test_file_operation.cpp
@@ -12,19 +12,26 @@ Project Type: Open-source
 int main() {
     std::cout<<"File I/O Operation Call" <<std::endl;
 
-    const char *file_name= "JustinTime";
-
+    const char *create_file_name= "create_file_name";
+    const char *open_file_name= "open_file_name";
+    /*
     check_file_permission_type(READ_WRITE);
     check_file_descriptor(CREATE_ONLY);
-    /*
-    create_new_file(file_name, MULTIPLE_FLAGS);
-    open_new_file(file_name, MULTIPLE_FLAGS);
     */
-   off_t seek_position = seek_file_to_offset(READ_ONLY, 0, SEEK_TO_CURR_VAL);
-   if(seek_position != -1) {
-       std::cout<<"Seeking to current file offset"<<std::endl;
-       std::cout<<seek_position<<std::endl;
-   }
+    
+    int fd = create_new_file(create_file_name, MULTIPLE_FLAGS); 
+    int fds = open_new_file(open_file_name, MULTIPLE_FLAGS);
+    std::cout<<"open_new_file Returned value: "<<fds<<std::endl;
+    
+    
+    off_t seek_position = seek_file_to_offset(fds, 0, SEEK_TO_CURR_VAL);
+    std::cout<<"seek_file_to_offset Returned: "<<seek_position<<std::endl;
+    if(seek_position != -1) {
+        std::cout<<"Seeking to current file offset"<<std::endl;
+        std::cout<<seek_position<<std::endl;
+    }
+    
+   
     
     
 
