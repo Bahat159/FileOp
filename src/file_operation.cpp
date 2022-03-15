be4bf79d7ef4c1f36b45bf3e9a173bf3a88e08a0
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-14 20:42:16 -0500

    Update file_operation.cpp

--- src/file_operation.cpp
+++ src/file_operation.cpp
@@ -1,7 +1,7 @@
 #include "file_io.h"
 
 
-static void create_new_file(const char *path, mode_t mode) {
+static int create_new_file(const char *path, mode_t mode) {
     /* 
     TODO
     Check if file already exist
@@ -11,7 +11,9 @@ static void create_new_file(const char *path, mode_t mode) {
         std::cout<<"["<<path<<"] created successfully with: "<<mode<<" Descriptor"<<std::endl;
     } else {
         std::cout<<"Error creating filename: "<<path<<std::endl;
+        return file_to_create;
     }
+    return file_to_create;
 }
 
 /* 
@@ -21,26 +23,34 @@ open(path, WRITE_ONLY);
 open(path, READ_ONLY);
 open(path, READ_WRITE);
 */
-static void open_new_file(const char *path, int oflag) {
+static int open_new_file(const char *path, int oflag) {
+    int opened_file;
     if (path && oflag != FILE_IO_NULL) {
-        int opened_file = open(path, oflag);
+        opened_file = open(path, oflag);
         if (opened_file) {
             std::cout<<"File opened successfully" <<std::endl;
+            return opened_file;
         } else if( opened_file == -1) {
             std::cout<<"Error opening file"<<std::endl;
+            return -1;
         }
     }
+    return opened_file;
 }
 
-static void open_file_with_fd(int fd, const char *path, int oflag) {
+static int open_file_with_fd(int fd, const char *path, int oflag) {
+    int open_with_fd;
     if (fd && path && oflag != FILE_IO_NULL) {
-        int open_with_fd = openat(fd, path, oflag);
+        open_with_fd = openat(fd, path, oflag);
         if(open_with_fd) {
             std::cout <<"File opened successfully"<<std::endl;
+            return open_with_fd;
         } else if(open_with_fd == -1) {
             std::cout<<"Error opening file with descriptor"<<std::endl;
+            return -1;
         }
     }
+    return open_with_fd;
 }
 
 static int close_opened_file(int fd) {
