0fde0185e96c6812cde70aeafa759dabb459bfad
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-15 04:53:36 -0500

    Update test_file_main.cpp

--- test/test_file_main.cpp
+++ test/test_file_main.cpp
@@ -9,10 +9,9 @@ Copyright: SandCroft Software
 Project Type: Open-source 
 */
 
-int main() {
+int main(int argc, char *argv[]) {
     std::cout<<"File I/O Operation Call" <<std::endl;
 
-    const char *create_file_name= "create_file_name";
     const char *open_file_name= "open_file_name";
 
     check_file_permission_type(FILE_IO_PERM_READ_WRITE);
@@ -24,9 +23,36 @@ int main() {
     char buf1[] = "abcdefghij";
     char buf2[] = "ABCDEFGHIJ";
 
-    int fd = create_new_file(create_file_name, FILE_IO_MULTIPLE_FLAGS); 
+    if (argc > 1){
+        int fd = create_new_file(argv[1], FILE_IO_MULTIPLE_FLAGS); 
+        std::cout<<"create_new_file returned flag: "<<fd<<std::endl;
 
-    
+        unsigned int write_file = write_to_file(fd, buf1, 10);
+        if (write_file != 10) {
+            std::cout <<"Write to buffer error"<<std::endl;
+        } else {
+            std::cout<<write_file<<" Bytes written to: "<<fd<<std::endl;
+        }
+        
+        off_t seek_position = seek_file_to_offset(fd, 16384, FILE_IO_SEEK_TO_CURR_VAL);
+        std::cout<<"seek_file_to_offset Returned: "<<seek_position<<std::endl;
+        if(seek_position != FILE_IO_E_ERROR) {
+            std::cout<<"Seeking to current file offset: "<<seek_position<<std::endl;
+        }
+        
+        unsigned int write_file0 = write_to_file(fd, buf2, 10);
+        if (write_file0 != 10) {
+            std::cout <<"Write to buffer error"<<std::endl;
+        } else {
+            std::cout<<write_file0<<" Bytes written to: "<<fd<<std::endl;
+        }
+
+    } else {
+        std::cout<<"Usage: ./file_io [ file_name ]"<<std::endl;
+        exit(-1);
+    }
+
+    /*
     
     unsigned int n = read_from_file(fd, buf, FILE_IO_BUFSIZE);
     if(n) {
@@ -36,29 +62,7 @@ int main() {
     int fds = open_new_file(open_file_name, FILE_IO_MULTIPLE_FLAGS);
     std::cout<<"open_new_file Returned value: "<<fds<<std::endl;
 
-    
-
-    unsigned int write_file = write_to_file(fd, buf1, 10);
-    if (write_file != 10) {
-        std::cout <<"Write to buffer error"<<std::endl;
-    } else {
-        std::cout<<write_file<<" Bytes written to: "<<fd<<std::endl;
-    }
-
-    off_t seek_position = seek_file_to_offset(fd, 16384, FILE_IO_SEEK_TO_CURR_VAL);
-    std::cout<<"seek_file_to_offset Returned: "<<seek_position<<std::endl;
-    if(seek_position != FILE_IO_E_ERROR) {
-        std::cout<<"Seeking to current file offset: "<<seek_position<<std::endl;
-    }
-
-    unsigned int write_file0 = write_to_file(fd, buf2, 10);
-    if (write_file0 != 10) {
-        std::cout <<"Write to buffer error"<<std::endl;
-    } else {
-        std::cout<<write_file0<<" Bytes written to: "<<fd<<std::endl;
-    }
-
-    
+    */
     
     
     
