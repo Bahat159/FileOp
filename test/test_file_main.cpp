201287c8c5ff428da6d15eb160b28a5075865fe2
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-15 11:39:16 -0500

    Update test_file_main.cpp

--- test/test_file_main.cpp
+++ test/test_file_main.cpp
@@ -1,6 +1,6 @@
 #include "../include/file_io.h"
 #include "../src/file_permission.cpp"
-#include "../src/file_operation.cpp"
+#include "../src/file_io_wrapper.cpp"
 
 /*
 Author: Busari Habibullaah
@@ -22,49 +22,25 @@ int main(int argc, char *argv[]) {
     char buf[FILE_IO_BUFSIZE];
     char buf1[] = "abcdefghij";
     char buf2[] = "ABCDEFGHIJ";
+    off_t pread_offset = 0;
+    off_t pwrite_offset = 1024;
+
 
     if (argc > 1){
         int fd = create_new_file(argv[1], FILE_IO_MULTIPLE_FLAGS); 
         std::cout<<"create_new_file returned flag: "<<fd<<std::endl;
-
-        unsigned int write_file = write_to_file(fd, buf1, 10);
-        if (write_file != 10) {
-            std::cout <<"Write to buffer error"<<std::endl;
-        } else {
-            std::cout<<write_file<<" Bytes written to: "<<fd<<std::endl;
-        }
         
-        off_t seek_position = seek_file_to_offset(fd, 16384, FILE_IO_SEEK_TO_CURR_VAL);
-        std::cout<<"seek_file_to_offset Returned: "<<seek_position<<std::endl;
-        if(seek_position != FILE_IO_E_ERROR) {
-            std::cout<<"Seeking to current file offset: "<<seek_position<<std::endl;
-        }
+        open_and_read_file(argv[2], buf);
+        write_seek_file_for_single_process(fd, buf1, buf2, 16384, 10);
+        write_read_file_for_multiple_process(fd, buf1, 10, pwrite_offset, pread_offset);
         
-        unsigned int write_file0 = write_to_file(fd, buf2, 10);
-        if (write_file0 != 10) {
-            std::cout <<"Write to buffer error"<<std::endl;
-        } else {
-            std::cout<<write_file0<<" Bytes written to: "<<fd<<std::endl;
-        }
 
     } else {
-        std::cout<<"Usage: ./file_io [ file_name ]"<<std::endl;
+        std::cout<<"Usage: ./file_io [ create_new_file name ] [ open_and_read_file name] optional"<<std::endl;
         exit(-1);
     }
 
-    /*
-    
-    unsigned int n = read_from_file(fd, buf, FILE_IO_BUFSIZE);
-    if(n) {
-        std::cout<<"read_from_file bytes read: "<<n<<std::endl;
-    }
 
-    int fds = open_new_file(open_file_name, FILE_IO_MULTIPLE_FLAGS);
-    std::cout<<"open_new_file Returned value: "<<fds<<std::endl;
-
-    */
-    
-    
-    
     return 0;
 }
+
