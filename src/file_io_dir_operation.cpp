115bc9c06b5bcd34552d595a74347afe15d15911
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-16 15:10:22 -0500

    Update file_io_dir_operation.cpp

--- src/file_io_dir_operation.cpp
+++ src/file_io_dir_operation.cpp
@@ -21,4 +21,15 @@ static int get_file_stat(const char *pathname, struct stat *buf) {
     return get_stat;
 }
 
-
+static int get_file_fstat(int fd, struct stat *buf) {
+    int get_fstat;
+    if(fd && buf != FILE_IO_DIR_NULL) {
+        get_fstat = fstat(fd, buf);
+        if(get_fstat == FILE_IO_DIR_NULL) {
+            return get_fstat;
+        } else {
+            return FILE_IO_E_ERROR;
+        }
+    }
+    return get_fstat;
+}
