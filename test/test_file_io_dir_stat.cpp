c839637575e3428f4f622e2342626a4a40b733d3
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-16 15:09:50 -0500

    Update test_file_io_dir_stat.cpp

--- test/test_file_io_dir_stat.cpp
+++ test/test_file_io_dir_stat.cpp
@@ -1,3 +1,5 @@
+
+#include "../src/file_operation.cpp"
 #include "../src/file_io_dir_operation.cpp"
 
 int main(int argc, char *argv[]){
@@ -9,6 +11,10 @@ int main(int argc, char *argv[]){
     if (argc > 1){
         int returned_value = get_file_stat (argv[1], &buf);
         std::cout<<"get_file_stat returned: "<< returned_value<<std::endl;
+
+        int fd = open_new_file(argv[2], FILE_IO_PERM_CREATE_ONLY);
+        int ret_fstat = get_file_fstat(fd, &buf);
+        std::cout<<"get_file_fstat returned: "<<ret_fstat<<std::endl;
 
     } else {
         std::cout<<"Usage: ./test_file_io_dir_stat [ file_name  ]"<<std::endl;
