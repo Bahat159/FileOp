6727fb7ee751e59750225eceb53707d6f9c2e668
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-16 12:24:33 -0500

    Update test_io_set_and_get_fd.cpp

--- test/test_io_set_and_get_fd.cpp
+++ test/test_io_set_and_get_fd.cpp
@@ -6,14 +6,14 @@ int main(int argc, char *argv[]) {
     std::cout<<"File I/O Operation Call" <<std::endl;
 
     if (argc > 1){
-        int fd = create_new_file(argv[1], FILE_IO_MULTIPLE_FLAGS); 
-        std::cout<<"create_new_file file descriptor returned: "<<fd<<std::endl;
+        int fd = open_new_file(argv[1], FILE_IO_MULTIPLE_FLAGS); 
+        std::cout<<"open_new_file file descriptor returned: "<<fd<<std::endl;
 
         set_and_get_file_fd_properties(fd, FILE_IO_FCNTL_CMD_DUPLICATE_FD);
 
-        int se_ = set_and_get_file_descriptor_with_cmd(fd, FILE_IO_FCNTL_CMD_DUPLICATE_FD);
+        int se_ = set_and_get_file_descriptor_with_cmd(fd, FILE_IO_FCNTL_CMD_GET_FD);
         std::cout<<"file access mode: " <<se_<<std::endl;
-        int record_l = set_and_get_file_descriptor_with_cmd(fd, FILE_IO_FCNTL_CMD_DUPLICATE_FD,FILE_IO_FCNTL_CMD_GET_FILE_STATUS_FLAG);
+        int record_l = set_and_get_file_descriptor_with_cmd(fd, FILE_IO_FCNTL_CMD_DUPLICATE_FD, FILE_IO_FCNTL_CMD_GET_FILE_STATUS_FLAG);
 
         std::cout<<"file access mode: " <<record_l<<std::endl;
 
