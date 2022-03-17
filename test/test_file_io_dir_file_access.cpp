528ae29c23f60e289f5c5cc2852f56a5eeb50362
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-17 06:18:56 -0500

    Create test_file_io_dir_file_access.cpp

---
+++ test/test_file_io_dir_file_access.cpp
@@ -0,0 +1,30 @@
+#include "../src/file_operation.cpp"
+#include "../src/file_io_dir_operation.cpp"
+/// #include "../src/file_io_dir_wrapper.cpp"
+
+int main(int argc, char *argv[]){
+    std::cout<<"File I/O Operation Call" <<std::endl;
+    std::cout<<"File stat Test"<<std::endl;
+
+
+    if (argc > 1){
+        
+        int access_read = set_file_access_mode(argv[1], FILE_IO_ACCESS_MODE_READ);
+
+        if(access_read == FILE_IO_E_NULL) {
+            std::cout<<"Access Read okay" <<std::endl;
+        }
+        std::cout<<"set_file_access_mode returned: "<<access_read<<std::endl;
+
+        int open_to_read = open_new_file(argv[1], FILE_IO_PERM_READ_ONLY);
+        if (open_to_read != FILE_IO_E_ERROR){
+            std::cout<<"["<<argv[1] <<"] Open for reading"<<std::endl;
+        }
+        std::cout<<"open_new_file returned: "<<open_to_read<<std::endl;
+
+    } else {
+        std::cout<<"Usage: ./test_file_io_dir_stat [ file_name  ]"<<std::endl;
+        exit(-1);
+    }
+    return 0;
+}
